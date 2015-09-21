58a4fb833edbe812fe960b1edba42f10c6cc6b5c
Brandon Borders <brandon@camelcased.com>
2015-09-21 12:11:52 -0700

    Add Projects state

--- client/index.js
+++ client/index.js
@@ -9,12 +9,42 @@ app.config(['$urlRouterProvider', '$stateProvider', '$locationProvider',
         url: '/',
         templateUrl: 'client/templates/home/index.ng.html',
         controller: 'HomeController'
+      })
+      .state('projects', {
+        url: '/projects',
+        templateUrl: 'client/templates/projects/index.ng.html',
+        controller: 'ProjectsController',
+        resolve: {
+          'currentUser': ['$meteor',
+            function($meteor) {
+              return $meteor.requireUser();
+            }
+          ]
+        }
       });
+
+    $urlRouterProvider.otherwise('/projects');
   }
 ]);
 
+app.run(['$rootScope', '$location',
+  function($rootScope, $location) {
+    $rootScope.$on('stateChangeError', function(event, next, previous, error) {
+      if (error === 'AUTH_REQUIRED') {
+        $state.go('/');
+      }
+    });
+  }
+])
+
 app.controller('HomeController', ['$scope',
   function($scope) {
+
+  }
+]);
+
+app.controller('ProjectsController', ['$scope', '$meteor',
+  function($scope, $meteor) {
 
   }
 ]);
