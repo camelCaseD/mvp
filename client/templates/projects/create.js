c45e306f6e6329b4d9dfcf264aedbfd685f9b112
Brandon Borders <brandon@camelcased.com>
2015-09-21 16:06:10 -0700

    Add buttons to create project view

--- client/templates/projects/create.js
+++ client/templates/projects/create.js
@@ -1,7 +1,7 @@
 angular.module('project-management.createProject', [])
 
-.controller('CreateProjectController', ['$scope',
-  function($scope) {
+.controller('CreateProjectController', ['$scope', '$state',
+  function($scope, $state) {
     $scope.colors = [{name: 'red', hex: '#F44336'},{name: 'purple', hex: '#673AB7'},
     {name: 'dark blue', hex: '#3F51B5'},{name: 'blue', hex: '#2196F3'},
     {name: 'cyan', hex: '#00BCD4'},{name: 'green', hex: '#4CAF50'},
@@ -9,6 +9,10 @@ angular.module('project-management.createProject', [])
 
     $scope.chooseColor = function(name) {
       $scope.chosenColor = name;
+    };
+
+    $scope.goToProjects = function() {
+      $state.go('projects');
     }
   }
 ]);
