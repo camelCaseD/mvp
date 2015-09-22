98fe91f3b7f00e12e73e807adce12f26e42adc2a
Brandon Borders <brandon@camelcased.com>
2015-09-22 15:39:01 -0700

    Setup Estimate view

--- client/templates/projects/estimate.js
+++ client/templates/projects/estimate.js
@@ -3,5 +3,10 @@ angular.module('project-management.estimate', [])
 .controller('EstimateController', ['$scope', '$state', '$meteor',
   function($scope, $state, $meteor) {
     $scope.project = $meteor.object(Projects, $state.params.projectId);
+
+    $scope.$meteorSubscribe('estimateData', $scope.project._id)
+      .then(function(handle) {
+        $scope.tasks = $meteor.collection(Tasks);
+      });
   }
 ]);
--- server/publications.js
+++ server/publications.js
@@ -13,3 +13,19 @@ Meteor.publish('subTasks', function(taskId) {
     return Tasks.find({taskId: taskId});
   }
 });
+
+Meteor.publish('estimateData', function(projectId) {
+  var projects = Projects.find();
+
+  if (projects.count() === 1) {
+    return Tasks.find({projectId: projectId});
+  } else {
+    var tasks = Tasks.find({taskId: {$exists: false}}).fetch();
+
+    for (var i = 0; i < tasks.length; i++) {
+      var task = tasks[i];
+
+      
+    }
+  }
+});
