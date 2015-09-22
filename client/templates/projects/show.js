90652f7d6fea53d16bc074072a4b9c68b616fac1
Brandon Borders <brandon@camelcased.com>
2015-09-21 17:02:12 -0700

    Retreive project for it's view

--- client/templates/projects/show.js
+++ client/templates/projects/show.js
@@ -1,7 +1,7 @@
 angular.module('project-management.showProject', [])
 
-.controller('ProjectController', ['$scope',
-  function($scope) {
-
+.controller('ProjectController', ['$scope', '$state', '$meteor',
+  function($scope, $state, $meteor) {
+    $scope.project = $meteor.object(Projects, $state.params._id);
   }
 ]);
