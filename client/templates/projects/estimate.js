b13b61bfad0516be0063415e9663c1b79042d797
Brandon Borders <brandon@camelcased.com>
2015-09-22 16:01:56 -0700

    Add buttons to estimate view

--- client/templates/projects/estimate.js
+++ client/templates/projects/estimate.js
@@ -2,11 +2,21 @@ angular.module('project-management.estimate', [])
 
 .controller('EstimateController', ['$scope', '$state', '$meteor',
   function($scope, $state, $meteor) {
+    var taskHandler;
+
     $scope.project = $meteor.object(Projects, $state.params.projectId);
 
     $scope.$meteorSubscribe('estimateData', $scope.project._id)
       .then(function(handle) {
         $scope.tasks = $meteor.collection(Tasks);
+
+        taskHandler = handle;
       });
+
+    $scope.cancel = function() {
+      taskHandler.stop();
+
+      $state.go('project', {_id: $scope.project._id});
+    }
   }
 ]);
