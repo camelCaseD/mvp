3f17547e636bb38b615534f258cc8dafd82371a9
Brandon Borders <brandon@camelcased.com>
2015-09-21 17:28:58 -0700

    Show controller subscribes to tasks

--- client/templates/projects/show.js
+++ client/templates/projects/show.js
@@ -3,5 +3,7 @@ angular.module('project-management.showProject', [])
 .controller('ProjectController', ['$scope', '$state', '$meteor',
   function($scope, $state, $meteor) {
     $scope.project = $meteor.object(Projects, $state.params._id);
+
+    $scope.tasks = $meteor.collection(Tasks).subscribe('tasks', $scope.project._id);
   }
 ]);
