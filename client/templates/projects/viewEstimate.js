aad0c2adb43ebc80da35923dd17e4275a368614f
Brandon Borders <brandon@camelcased.com>
2015-09-22 18:15:14 -0700

    Initial View Estimate view

--- client/templates/projects/viewEstimate.js
+++ client/templates/projects/viewEstimate.js
@@ -7,6 +7,19 @@ angular.module('project-management.viewEstimate', [])
         Tracker.autorun(function(){
           $scope.estimate = Estimates.findOne({_id: $state.params.estimateId});
         });
+
+        $scope.$$subTasks();
       });
+
+    $scope.$$subTasks = function() {
+      $scope.$meteorSubscribe('tasks', $scope.estimate.projectId)
+        .then(function(handle) {
+          Tracker.autorun(function(){
+            $scope.tasks = Tasks.find({projectId: $scope.estimate.projectId}).fetch();
+
+            console.log($scope.tasks)
+          });
+        });
+    };
   }
 ]);
